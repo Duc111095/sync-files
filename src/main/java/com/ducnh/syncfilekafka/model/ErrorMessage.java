6576f8b9da76ce215a096dabb45cb83e1b187a93
Duc111095 <huynhduc111095@gmail.com>
2025-10-25 22:56:50 +0700

    fix builder errorMessage class

--- src/main/java/com/ducnh/syncfilekafka/model/ErrorMessage.java
+++ src/main/java/com/ducnh/syncfilekafka/model/ErrorMessage.java
@@ -25,20 +25,29 @@ public class ErrorMessage {
 			this.errorMessage = new ErrorMessage();
 		}
 		
-		public void uuid(String uuid) {
+		public Builder uuid(String uuid) {
 			this.errorMessage.uuid = uuid;
+			return this;
 		}
 		
-		public void status(char status) {
+		public Builder errMsg(String errMsg) {
+			this.errorMessage.errMsg = errMsg;
+			return this;
+		}
+		
+		public Builder status(char status) {
 			this.errorMessage.status = status;
+			return this;
 		}
 		
-		public void createdDate(LocalDateTime createdDate) {
+		public Builder createdDate(LocalDateTime createdDate) {
 			this.errorMessage.createdDate = createdDate;
+			return this;
 		}
 		
-		public void infos(Map<String, Object> infos) {
+		public Builder infos(Map<String, Object> infos) {
 			this.errorMessage.infos = infos;
+			return this;
 		}
 		
 		public ErrorMessage build() {
